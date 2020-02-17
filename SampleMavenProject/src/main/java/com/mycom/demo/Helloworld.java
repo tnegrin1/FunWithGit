b96d6eafa07c8051f7cb9351570964db43b29079
Tomer Negrin <tomer.negrin@amdocs.com>
2020-02-17 12:39:14 -0500

    maven project

---
+++ SampleMavenProject/src/main/java/com/mycom/demo/Helloworld.java
@@ -0,0 +1,10 @@
+package com.mycom.demo;
+
+import sun.applet.AppletSecurity;
+
+public class Helloworld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
